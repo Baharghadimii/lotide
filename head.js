78795b4d450863372213d1010ccd03464941deb4
Baharehghadimii <bahar.ghadimi5469@gmail.com>
2019-10-15 00:05:04 +0000

    add head function

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\uD83D\uDC36 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function (array) {
+  return array[0];
+
+};
+
