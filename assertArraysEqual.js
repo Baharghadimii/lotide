58353b81c4a6b22f43446f62868ebb195da971b3
Baharehghadimii <bahar.ghadimi5469@gmail.com>
2019-10-16 01:50:31 +0000

    add assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+
+};
+const assertArraysEqual = function (arr1,arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`\uD83D\uDC36 Assertion Failed: ${arr1} === ${arr2}`);
+  }
+};
+assertArraysEqual([1,2,4],[1,2,3]);
