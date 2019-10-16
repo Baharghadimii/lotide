36b449df3224b8b2afe662b2cf04b45c12cd8c01
Baharehghadimii <bahar.ghadimi5469@gmail.com>
2019-10-16 01:31:46 +0000

    add eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\uD83D\uDC36 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+
+};
