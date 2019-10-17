21678a95db1934b44c294dc47bb8174df1baf6d3
Baharehghadimii <bahar.ghadimi5469@gmail.com>
2019-10-17 01:43:22 +0000

    add assertObjectsEqual function

--- eqObjects.js
+++ eqObjects.js
@@ -10,37 +10,45 @@ const assertEqual = function (actual, expected) {
 };
 const eqArrays = function (arr1, arr2) {
   if (arr1.length === arr2.length) {
-
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         return false;
+      } else {
+        return true;
       }
     }
   } else {
     return false;
   }
-  return true;
 
 };
 
 const eqObjects = function (obj1, obj2) {
-  let codition;
-  for (let key in obj1) {
-    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-      codition = eqArrays(obj1[key], obj2[key]);
-    } else {
-      if (obj1[key] === obj2[key]) {
-        codition = true;
+  if (obj1.length === obj2.length) {
+    for (let key in obj1) {
+      if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+        return eqArrays(obj1[key], obj2[key]);
       } else {
-        codition = false;
+        if (obj1[key] !== obj2[key]) {
+          return false;
+        }
+
       }
-    }
+    } return true;
+  } else {
+    return false;
   }
-  return codition;
 };
+
+
+
+
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+// console.log(eqObjects(cd, dc)); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(eqObjects(cd, cd2)); // => false
+console.log(eqObjects({ a: 1, b: 2 }, { b: 2, a: 1 })); //true
+
+console.log(eqObjects({ a: 1, b: 2 }, { b: 2, a: 2 }));
