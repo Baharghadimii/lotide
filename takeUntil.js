afc51a435e84e592f2ec230d1f7d820a4a14859e
Baharehghadimii <bahar.ghadimi5469@gmail.com>
2019-10-17 22:42:52 +0000

    add take util function

---
+++ takeUntil.js
@@ -0,0 +1,41 @@
+const takeUntil = function (array, callback) {
+  const temp = [];
+  for (const item in array) {
+    if (!callback(array[item])) {
+      temp.push(array[item]);
+    } else {
+      return temp;
+    }
+  }
+};
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+
+};
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`\uD83D\uDC36 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood', "hello"]);
