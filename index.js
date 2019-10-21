b5f8a4c29d08a791e98c6ee11d15705be2f87a29
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-10-21 13:39:33 -0700

    add index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetter = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUtil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetter,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUtil,
+  without
+
+};
