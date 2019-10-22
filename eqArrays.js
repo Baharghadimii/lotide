446fb4cc14e93c053a585ed94630bab7765332ef
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-10-21 20:35:45 -0700

    bug fixes

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,17 @@
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   const index = require('./index');
   let temp1 = index.flatten(arr1);
   let temp2 = index.flatten(arr2);
-  
-  if (temp1.length === temp2.length) {
-    temp1.forEach((element, index) => {
-      if (element !== temp2[index]) {
-        return false;
-      }
-    });
 
-    return true;
-  } else {
+  if (temp1.length !== temp2.length)
     return false;
+  for (let i = temp1.length; i--;) {
+    if (temp1[i] !== temp2[i])
+      return false;
   }
+  return true;
 
 };
+
 
 module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -5,10 +5,12 @@ describe('EqualArrays', function() {
   it('should return true if two flat arrays are the same', function() {
     assert.deepEqual(index.eqArrays(["hello" , "world"],["hello" , "world"]), true);
     assert.deepEqual(index.eqArrays([1, 2, 3], [1, 2, 3]), true);
-
   });
   it('should return true if two nested arrays are the same', function() {
     assert.deepEqual(index.eqArrays([1, [2, [3]], 3], [1, [2, [3]], 3]), true);
   });
+});
+it('should return false if two arrays are not the same', function() {
+  assert.deepEqual(index.eqArrays([1, 2, 4], [1, 2, 3]), false);
 });
 
